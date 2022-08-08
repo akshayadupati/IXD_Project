233e2c49647cc65803fc3a2380320458b365d311
Akshaya Dupati <98553741+akshayadupati@users.noreply.github.com>
2022-08-08 15:05:37 -0400

    added forum toggle

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Forum.scss";
+import nativeImage from "../../../src/nativeImage.png";
+import banglaImage from "../../../src/banglaImage.png";
 
 function Forum() {
-  return <h1>Forum Page</h1>;
+  const [imageState, setImageState] = useState(true);
+
+  const handleImage = (val) => {
+    if (val) {
+      setImageState(true);
+    } else {
+      setImageState(false);
+    }
+  };
+  return (
+    <div>
+      <div className="btn-group">
+        <button onClick={() => handleImage(true)}>English</button>
+        <button onClick={() => handleImage(false)}>Bangla</button>
+      </div>
+      <div className="img-class">
+        <img
+          className="mail-image"
+          src={imageState ? nativeImage : banglaImage}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Forum;
